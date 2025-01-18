5820792e8275f0e3ccc3a672ba41cc7d1ca6b86c
Poojha Prasanna <146710301+Poojha-prasanna10@users.noreply.github.com>
2025-01-18 23:09:22 +0530

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,1 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
 
+# Load CIFAR-10 data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+X_train, X_test = X_train / 255.0, X_test / 255.0  # Normalize to [0, 1]
+y_train, y_test = to_categorical(y_train, 10), to_categorical(y_test, 10)
+
+# Build the model
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    MaxPooling2D((2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')  # 10 output classes for CIFAR-10
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, epochs=10, batch_size=64, validation_data=(X_test, y_test))
+
+# Save the model
+model.save('models/model.h5')
